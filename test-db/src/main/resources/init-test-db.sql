7010760d1f6758ed3098eea3ce09104e13b13d00
Aleks-ir <aleksir.com@gmail.com>
2021-10-09 20:53:00 +0300

    update test-db

---
+++ test-db/src/main/resources/init-test-db.sql
@@ -0,0 +1,14 @@
+INSERT INTO USERS (user_id, user_name, user_email, user_money) VALUES (1, 'Magnetbro', 'magnetbro@gmail.com', 50);
+INSERT INTO USERS (user_id, user_name, user_email, user_money) VALUES (2, 'Angelina Brolie', 'angelinabrolie@gmail.com', 23.5);
+INSERT INTO USERS (user_id, user_name, user_email, user_money) VALUES (3, 'Brogi Berra', 'brogiberra@gmail.com', 10.2);
+INSERT INTO USERS (user_id, user_name, user_email, user_money) VALUES (4, 'Rimbraud', 'rimbraud@gmail.com', 65.7);
+INSERT INTO USERS (user_id, user_name, user_email, user_money) VALUES (5, 'Brofessor X', 'brofessorx@gmail.com', 104);
+
+INSERT INTO PRODUCTS (product_id, product_name, product_price, product_date, salesman_id) VALUES (1, 'Dresse', 10, '2022-01-07', 2);
+INSERT INTO PRODUCTS (product_id, product_name, product_price, product_date, salesman_id) VALUES (2, 'Shirt', 6.11, '2022-01-07', 2);
+INSERT INTO PRODUCTS (product_id, product_name, product_price, product_date, salesman_id) VALUES (3, 'Jeans Regular Hem', 14.12, '2022-01-07', 1);
+INSERT INTO PRODUCTS (product_id, product_name, product_price, product_date, salesman_id) VALUES (4, 'Double Stich Hem', 15.3, '2022-01-06', 3);
+INSERT INTO PRODUCTS (product_id, product_name, product_price, product_date, salesman_id) VALUES (5, 'Comforter - Dry Clean', 31.95, '2022-01-06', 4);
+INSERT INTO PRODUCTS (product_id, product_name, product_price, product_date, salesman_id) VALUES (6, 'Button', 2, '2022-01-07', 5);
+INSERT INTO PRODUCTS (product_id, product_name, product_price, product_date, salesman_id) VALUES (7, 'Zipper Replacement', 16.05, '2022-01-07', 5);
+INSERT INTO PRODUCTS (product_id, product_name, product_price, product_date, salesman_id) VALUES (8, 'Business Shirt', 3.45, '2022-01-06', 1);
